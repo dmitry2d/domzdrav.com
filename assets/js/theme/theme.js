a3ccc38ad93d24a0c26f34af07aa20ada2ba7923
Dmitry <volcharo@gmail.com>
2024-09-17 12:27:20 +0300

    code

---
+++ assets/js/theme/theme.js
@@ -0,0 +1,4 @@
+
+
+import './settings.js';
+import './common.js';
---
+++ components/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon_package/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon_package/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon_package/favicon-16x16.png">
+    <link rel="manifest" href="./assets/favicon_package/site.webmanifest">
+    <link rel="mask-icon" href="./assets/favicon_package/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <title>Медицинский центр "Дом Здоровья" на Зеленой, Великий Новгород</title>
+    
+    <!-- Bootstrap -->
+    <link href="./assets/css/custom.min.css" rel="stylesheet">
+    <script src="./assets/js/bootstrap.bundle.js"></script>
+    <!-- /Bootstrap -->
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.3/jquery-ui.min.js"></script>
+    <!-- /jQuery -->
+
+    <!-- Lightbox -->
+    <link href="./assets/lightbox/css/lightbox.css" rel="stylesheet" />
+    <script src="./assets/lightbox/js/lightbox.js"></script>
+    <!-- /Lightbox -->
+
+    <!-- Slick slider -->
+    <link href="./assets/slick/slick.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="./assets/slick/slick.min.js"></script>
+    <!-- /Slick slider -->
+
+    <!-- Theme scripts -->
+    <script type="module" src="assets/js/theme/theme.js"></script>
+    <!-- /Theme scripts -->
+
+</head>
+<body>
